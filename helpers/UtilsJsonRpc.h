0b0eef88daf134c642e01720e507b01f4030874e
Michael Smith <michael_smith2@comcast.com>
2026-02-26 20:24:11 +0000

    Cleaning up returnResponse comment

--- helpers/UtilsJsonRpc.h
+++ helpers/UtilsJsonRpc.h
@@ -38,7 +38,7 @@
         LOGTRACEMETHODFIN(); \
         return (successBoolean ? WPEFramework::Core::ERROR_NONE : WPEFramework::Core::ERROR_GENERAL); \
     }
-// returnResponse macro after applying patch '0001-RDKTV-20749-Revert-Merge-pull-request-3336-from-npol.patch', use 'originalReturnResponse' for original bahavior
+// This is the legacy behavior of returnResponse.  Once Web Apps can properly catch exceptions to handle an error return, the the above originalReturnResponse() can be used.
 #define returnResponse(success) \
     { \
         response["success"] = success; \
