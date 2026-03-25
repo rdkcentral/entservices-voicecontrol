62671c247b8734ec37ea55fb08ba49c19fa9d54b
Michael Smith <michael_smith2@comcast.com>
2026-03-25 19:42:57 +0000

    Updates

--- plugin/VoiceControlImplementation.cpp
+++ plugin/VoiceControlImplementation.cpp
@@ -21,7 +21,6 @@
 #include "PluginVersion.h"
 #include "libIBusDaemon.h"
 #include "UtilsIarm.h"
-#include "UtilsJsonRpc.h"
 
 #include <algorithm>
 
@@ -75,10 +74,14 @@ namespace Plugin {
 
         if (InitializeIARM() == false) {
             LOGERR("Failed to initialize IARM for VoiceControlImplementation, configuration will fail");
+            _service->Release();
+            _service = nullptr;
             return Core::ERROR_GENERAL;
         }
         if (Utils::IARM::isConnected() == false) {
             LOGERR("Failed to initialize IARM for VoiceControlImplementation, configuration will fail");
+            _service->Release();
+            _service = nullptr;
             return Core::ERROR_GENERAL;
         }
 
