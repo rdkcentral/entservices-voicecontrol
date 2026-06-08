39c9916d5412bbbc0ceddfe10c07807cf3688cf2
Michael Smith <michael_smith2@comcast.com>
2026-06-08 14:52:04 -0400

    RDKEMW-996: Add COM-RPC support to VoiceControl plugin (#4)

--- plugin/VoiceControl.h
+++ plugin/VoiceControl.h
@@ -1,95 +1,152 @@
-/**
-* If not stated otherwise in this file or this component's LICENSE
-* file the following copyright and licenses apply:
-*
-* Copyright 2024 RDK Management
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
+ /*
+  * If not stated otherwise in this file or this component's LICENSE file the
+  * following copyright and licenses apply:
+  *
+  * Copyright 2026 RDK Management
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 #pragma once
 
 #include "Module.h"
-#include "libIBus.h"
-
-#include "ctrlm_ipc.h"
-#include "ctrlm_ipc_voice.h"
-
-#define IARM_VOICECONTROL_PLUGIN_NAME       "Voice_Control"
+#include <interfaces/IVoiceControl.h>
+#include <interfaces/json/JVoiceControl.h>
+#include <interfaces/IConfiguration.h>
 
 namespace WPEFramework {
+namespace Plugin {
 
-    namespace Plugin {
+    class VoiceControl : public PluginHost::IPlugin, public PluginHost::JSONRPC {
+    public:
+        VoiceControl(const VoiceControl&) = delete;
+        VoiceControl& operator=(const VoiceControl&) = delete;
 
-        class VoiceControl : public PluginHost::IPlugin, public PluginHost::JSONRPC {
-        private:
-            // We do not allow this plugin to be copied !!
-            VoiceControl(const VoiceControl&) = delete;
-            VoiceControl& operator=(const VoiceControl&) = delete;
+        VoiceControl()
+            : _adminLock()
+            , _implementation(nullptr)
+            , _connectionId(0)
+            , _service(nullptr)
+            , _isShuttingDown(false)
+            , _connectionNotification(this)
+            , _notification(this)
+        {
+        }
+        ~VoiceControl() override = default;
 
-            //Begin methods
-            uint32_t getApiVersionNumber(const JsonObject& parameters, JsonObject& response);
-            void     sendNotify_(const char* eventName, JsonObject& parameters);
-
-            uint32_t voiceStatus(const JsonObject& parameters, JsonObject& response);
-            uint32_t configureVoice(const JsonObject& parameters, JsonObject& response);
-            uint32_t setVoiceInit(const JsonObject& parameters, JsonObject& response);
-            uint32_t sendVoiceMessage(const JsonObject& parameters, JsonObject& response);
-            uint32_t voiceSessionByText(const JsonObject& parameters, JsonObject& response); // DEPRECATED
-            uint32_t voiceSessionTypes(const JsonObject& parameters, JsonObject& response);
-            uint32_t voiceSessionRequest(const JsonObject& parameters, JsonObject& response);
-            uint32_t voiceSessionTerminate(const JsonObject& parameters, JsonObject& response);
-            uint32_t voiceSessionAudioStreamStart(const JsonObject& parameters, JsonObject& response);
-            //End methods
-
-            //Begin events
-            void onSessionBegin(ctrlm_voice_iarm_event_json_t* eventData);
-            void onStreamBegin(ctrlm_voice_iarm_event_json_t* eventData);
-            void onKeywordVerification(ctrlm_voice_iarm_event_json_t* eventData);
-            void onServerMessage(ctrlm_voice_iarm_event_json_t* eventData);
-            void onStreamEnd(ctrlm_voice_iarm_event_json_t* eventData);
-            void onSessionEnd(ctrlm_voice_iarm_event_json_t* eventData);
-            //End events
-
-        public:
-            VoiceControl();
-            virtual ~VoiceControl();
-            //IPlugin methods
-            virtual const string Initialize(PluginHost::IShell* service) override;
-            virtual void Deinitialize(PluginHost::IShell* service) override;
-            virtual string Information() const override { return {}; }
-
-            BEGIN_INTERFACE_MAP(VoiceControl)
+        BEGIN_INTERFACE_MAP(VoiceControl)
             INTERFACE_ENTRY(PluginHost::IPlugin)
             INTERFACE_ENTRY(PluginHost::IDispatcher)
+            INTERFACE_AGGREGATE(Exchange::IVoiceControl, _implementation)
+        END_INTERFACE_MAP
+
+        // IPlugin methods
+        const string Initialize(PluginHost::IShell* service) override;
+        void Deinitialize(PluginHost::IShell* service) override;
+        string Information() const override { return {}; }
+
+    private:
+        class ConnectionNotification : public RPC::IRemoteConnection::INotification {
+        public:
+            explicit ConnectionNotification(VoiceControl* parent) : _parent(*parent) {}
+            ~ConnectionNotification() override = default;
+
+            void Activated(RPC::IRemoteConnection*) override {}
+            void Deactivated(RPC::IRemoteConnection* connection) override {
+                _parent.Deactivated(connection);
+            }
+
+            BEGIN_INTERFACE_MAP(ConnectionNotification)
+                INTERFACE_ENTRY(RPC::IRemoteConnection::INotification)
             END_INTERFACE_MAP
 
         private:
-            void InitializeIARM();
-            void DeinitializeIARM();
-            // Handlers for ControlMgr BT Remote events
-            static void voiceEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
-            void iarmEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
-
-            // Local utility methods
-            void setApiVersionNumber(uint32_t apiVersionNumber);
-            void getMaskPii_();
-        public:
-            static VoiceControl* _instance;
-        private:
-            uint32_t m_apiVersionNumber;
-            bool     m_hasOwnProcess;
-            bool     m_maskPii;
+            VoiceControl& _parent;
         };
-	} // namespace Plugin
+
+        class Notification : public Exchange::IVoiceControl::INotification {
+        public:
+            explicit Notification(VoiceControl* parent) : _parent(*parent) {}
+            ~Notification() override = default;
+
+            void OnSessionBegin(const uint32_t remoteId, const string& sessionId, const Exchange::DeviceType deviceType, const bool keywordVerification) override {
+                Exchange::JVoiceControl::Event::OnSessionBegin(_parent, remoteId, sessionId, deviceType, keywordVerification);
+            }
+            void OnStreamBegin(const uint32_t remoteId, const string& sessionId) override {
+                Exchange::JVoiceControl::Event::OnStreamBegin(_parent, remoteId, sessionId);
+            }
+            void OnKeywordVerification(const uint32_t remoteId, const string& sessionId, const bool verified) override {
+                Exchange::JVoiceControl::Event::OnKeywordVerification(_parent, remoteId, sessionId, verified);
+            }
+            void OnServerMessage(const string& msgType, const string& trx, const uint64_t created, const string& msgPayload) override {
+                Exchange::JVoiceControl::Event::OnServerMessage(_parent, msgType, trx, created, msgPayload);
+            }
+            void OnStreamEnd(const uint32_t remoteId, const string& sessionId, const uint8_t reason) override {
+                Exchange::JVoiceControl::Event::OnStreamEnd(_parent, remoteId, sessionId, reason);
+            }
+            void OnSessionEnd(const uint32_t remoteId, const string& sessionId, const Exchange::SessionResult result, const Exchange::ServerStats& serverStats, const string& success, const string& error, const string& abort, const string& shortUtterance, const string& stbStats) override {
+                // Build params directly so optional opaque fields are only included when non-empty
+                JsonData::VoiceControl::OnSessionEndParamsData params;
+                params.RemoteId = remoteId;
+                params.SessionId = sessionId;
+                params.Result = result;
+                // Only include serverStats when the session completed (success/error).
+                // Abort events from ctrlm never include serverStats in the wire format.
+                if (result != Exchange::SessionResult::ABORT) {
+                    params.ServerStats = serverStats;
+                }
+                if (!success.empty()) {
+                    params.Success = success;
+                    params.Success.SetQuoted(false);
+                }
+                if (!error.empty()) {
+                    params.Error = error;
+                    params.Error.SetQuoted(false);
+                }
+                if (!abort.empty()) {
+                    params.Abort = abort;
+                    params.Abort.SetQuoted(false);
+                }
+                if (!shortUtterance.empty()) {
+                    params.ShortUtterance = shortUtterance;
+                    params.ShortUtterance.SetQuoted(false);
+                }
+                if (!stbStats.empty()) {
+                    params.StbStats = stbStats;
+                    params.StbStats.SetQuoted(false);
+                }
+                Exchange::JVoiceControl::Event::OnSessionEnd(_parent, params);
+            }
+
+            BEGIN_INTERFACE_MAP(Notification)
+                INTERFACE_ENTRY(Exchange::IVoiceControl::INotification)
+            END_INTERFACE_MAP
+
+        private:
+            VoiceControl& _parent;
+        };
+
+        void Deactivated(RPC::IRemoteConnection* connection);
+
+        Core::CriticalSection _adminLock;
+        Exchange::IVoiceControl* _implementation;
+        uint32_t _connectionId;
+        PluginHost::IShell* _service;
+        bool _isShuttingDown;
+        Core::Sink<ConnectionNotification> _connectionNotification;
+        Core::Sink<Notification> _notification;
+        Exchange::IConfiguration* _configure{};
+    };
+
+} // namespace Plugin
 } // namespace WPEFramework
