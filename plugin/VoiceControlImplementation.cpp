e0e68a1589f9fe065fe47ba0d651498abc290fb9
Michael Smith <michael_smith2@comcast.com>
2026-07-21 15:23:06 -0400

    RDKEMW-20502: Responses for Voice Control Thunder Test Client APIs are not being logged in WPE during API execution (#14)

--- plugin/VoiceControlImplementation.cpp
+++ plugin/VoiceControlImplementation.cpp
@@ -557,6 +557,7 @@ namespace Plugin {
         }
 
         result.FromString(call->result);
+        LOGINFO("%s Bus Call SUCCESS response=%.200s", method.c_str(), _maskPii ? "<***>" : call->result);
         free(call);
         return Core::ERROR_NONE;
     }
@@ -567,6 +568,7 @@ namespace Plugin {
     {
         response.version = API_VERSION_NUMBER_MAJOR;
         response.success = true;
+        LOGINFO("response: version=%u, success=%s", response.version, response.success ? "true" : "false");
         return Core::ERROR_NONE;
     }
 
