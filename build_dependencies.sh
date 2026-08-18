33f30e437ae2bc1bd54953cf9115c5e69abccb4b
RajaLucy <133349260+RajaLucy@users.noreply.github.com>
2026-08-18 22:39:06 +0530

    RDKEMW-22169:Updated version to 4.4.6 (#16)
    * RDKEMW-22169:Updated version to 4.4.6
    
    * RDKEMW-22169:Updated testframework ref and patches to R4.4.6
    
    * RDKEMW-22169: Update testframework version
    
    * RDKEMW-22169:Reverted json file
    
    * RDKEMW-22169: Updating Thunder branch and sha value
    
    * RDKEMW-22169: Fix WorkFlow Failures
    
    * RDKEMW-22169: Fixing L2 WorkFlow Failure
    
    * RDKEMW-22169:Update build_dependencies.sh
    
    * RDKEMW-22169:Fixing workflow failuers
    
    * RDKEMW-22169:Update build_dependencies.sh
    
    ---------
    
    Co-authored-by: srinibas15 <113517102+srinibas15@users.noreply.github.com>
    Co-authored-by: smohap466_comcast <srinibas_mohapatra@comcast.com>

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -2,6 +2,8 @@
 set -x
 set -e
 ##############################
+THUNDER_TOOLS_COMMIT_SHA="d5dd83c7c19c49c7f25c558c126500bd2d64f7a4"
+THUNDER_COMMIT_SHA="2c0fcc5529e7da734be558ca6efa05d934dcce31"
 GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 cd ${GITHUB_WORKSPACE}
@@ -35,22 +37,29 @@ cd ..
 # Clone the required repositories
 
 
-git clone --depth 1 --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
+git clone --depth 1 --branch  R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
+cd ThunderTools
+git fetch --depth 1 origin $THUNDER_TOOLS_COMMIT_SHA
+git checkout $THUNDER_TOOLS_COMMIT_SHA
+cd ..
 
-git clone --depth 1 --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
+git clone --depth 1 --branch R4_4-RDK https://github.com/rdkcentral/Thunder.git
+cd Thunder
+git fetch --depth 1 origin $THUNDER_COMMIT_SHA
+git checkout $THUNDER_COMMIT_SHA
+cd ..
 
-git clone --depth 1 --branch 4.0.8 https://github.com/rdkcentral/entservices-apis.git
+git clone --depth 1 --branch develop https://github.com/rdkcentral/entservices-apis.git
 
 git clone --depth 1 --branch $CTRLM_TAG https://github.com/rdkcentral/control.git
 
-git clone --depth 1 https://$GITHUB_TOKEN@github.com/rdkcentral/entservices-testframework.git
+git clone --depth 1 --branch 2.0.0 https://$GITHUB_TOKEN@github.com/rdkcentral/entservices-testframework.git
 
 ############################
 # Build Thunder-Tools
 echo "======================================================================================"
 echo "building thunderTools"
 cd ThunderTools
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4-Add-support-for-project-dir.patch
 cd -
 
 
@@ -69,10 +78,6 @@ echo "==========================================================================
 echo "building thunder"
 
 cd Thunder
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch
 cd -
 
 cmake -G Ninja -S Thunder -B build/Thunder \
