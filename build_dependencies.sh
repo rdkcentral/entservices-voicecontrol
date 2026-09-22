40148e9e7f4a5feb386d58fe992a433f4a3b4d30
Michael Smith <michael_smith2@comcast.com>
2026-09-22 19:12:47 +0000

    Prep for merge

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -47,7 +47,7 @@ cd Thunder
 git checkout 2c0fcc5529e7da734be558ca6efa05d934dcce31
 cd ..
 
-git clone --single-branch --depth 1 --branch feature/RDKEMW-22650 https://github.com/rdkcentral/entservices-apis.git
+git clone --single-branch --depth 1 --branch develop https://github.com/rdkcentral/entservices-apis.git
 
 git clone --single-branch --depth 1 --branch $CTRLM_TAG https://github.com/rdkcentral/control.git
 
